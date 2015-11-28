cfb9f65ab2aca56f36a6ca5cdf66dcd6b834b1f3
Robert-2360 <ROBERT40@live.seminolestate.edu>
2015-11-27 19:40:35 -0500

    added NonCompliantWordException.cs

---
+++ Hangman Game/NonCompliantWordException.cs
@@ -0,0 +1,17 @@
+﻿// Final Project: Hangman Game
+// Non Compliant Word Exception
+
+using System;
+
+namespace Hangman_Game
+{
+   class NonCompliantWordException : Exception
+   {
+      public NonCompliantWordException()
+         : base("Secret Word does not comply with the game system rules") { }
+
+      public NonCompliantWordException(string message) : base(message) { }
+
+      public NonCompliantWordException(string message, Exception inner) : base(message, inner) { }
+   }
+}
--- Hangman Game/WordFile.cs
+++ Hangman Game/WordFile.cs
@@ -3,14 +3,58 @@
 // Places a text file into a string array for the game
 
 using System;
+using System.Windows.Forms;
 
 namespace Hangman_Game
 {
    public class WordFile
    {
+      private string _secretWord;
       private string[] words;
+      private int minimumLength = 4;
+      private int maximumLength = 9;
 
-      public string SecretWord { get; set; }
+      public string SecretWord
+      {
+         get
+         {
+            return _secretWord;
+         }
+
+         set
+         {
+            try
+            {
+               if (value.Length < minimumLength)
+               {
+                  string message = string.Format("Length must be at least {0} characters long.", minimumLength);
+                  throw new NonCompliantWordException(message);
+               }
+               if (maximumLength < value.Length)
+               {
+                  string message = string.Format("Length must not be greater than {0} characters long.", maximumLength);
+                  throw new NonCompliantWordException(message);
+               }
+               if (!isAllLowerCase(value))
+               {
+                  string message = string.Format("Word contains at least one non-lowercase character.");
+                  throw new NonCompliantWordException(message);
+               }
+               else
+               {
+                  _secretWord = value;
+               }
+            }
+            catch (NonCompliantWordException ex)
+            {
+               string message =
+                  string.Format("Secret Word: {0}\n {1}\n Please correct this and rebuild.\n Program will now exit.",
+                  value, ex.Message);
+               MessageBox.Show(message);
+               Application.Exit();
+            }
+         }
+      }
 
       public WordFile()
       {
@@ -29,6 +73,21 @@ namespace Hangman_Game
          Random random = new Random();
          int randomNumber = random.Next(words.Length);
          SecretWord = words[randomNumber];
+      }
+
+      // Returns true if all the characters in the given word are lower case
+      public bool isAllLowerCase(string word)
+      {
+         bool result = true;
+         foreach (char c in word)
+         {
+            if (!Char.IsLower(c))
+            {
+               result = false;
+               break;
+            }
+         }
+         return result;
       }
    }
 }
